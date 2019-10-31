f5b0541c4765f95af2a75a2e7d634d9e678d8cdc
amit nautiyal <amitnautiyaldreamcoding@gmail.com>
2019-10-31 02:24:27 +0100

    Add files via upload

---
+++ stack-drift-notifier/lambda/drift_detection/lambda_function.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+
+import boto3
+import json
+import os
+import re
+from botocore.exceptions import ClientError
+from datetime import datetime, timedelta, timezone
+from itertools import repeat
+from multiprocessing import Pool
+from time import sleep
+
+from decorators import retry
+from sns_logger import SNSlogger
+
+
+ACTIVE_STACK_STATUS=[
+    'CREATE_IN_PROGRESS',
+    'CREATE_FAILED',
+    'CREATE_COMPLETE',
+    'ROLLBACK_IN_PROGRESS',
+    'ROLLBACK_FAILED',
+    'ROLLBACK_COMPLETE',
+    'UPDATE_IN_PROGRESS',
+    'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS',
+    'UPDATE_COMPLETE',
+    'UPDATE_ROLLBACK_IN_PROGRESS',
+    'UPDATE_ROLLBACK_FAILED',
+    'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS',
+    'UPDATE_ROLLBACK_COMPLETE',
+    'REVIEW_IN_PROGRESS'
+]
+
+REGIONS = [
+    'ap-south-1',
+    'eu-west-3',
+    'eu-west-2',
+    'eu-west-1',
+    'ap-northeast-2',
+    'ap-northeast-1',
+    'sa-east-1',
+    'ca-central-1',
+    'ap-southeast-1',
+    'ap-southeast-2',
+    'eu-central-1',
+    'us-east-1',
+
+]
+
+class DriftDetector(object):
+    '''
+    DriftDetector is a class that runs drift detection
+    on every stack and sends an SNS push for any resource not in sync.
+
+    Attributes:
+        stacks  List of Stack Objects
+    '''
+    stacks = []
+    failed_stack_ids = []
+
+    def __init__(self,profile="tr-fr-sandbox",region="us-east-1"):
+        self.sns_topic = os.environ.get('SNS_TOPIC_ID',None)
+        self.sns_subject = os.environ.get('SNS_SUBJECT',"CFN Drift Detector Report")
+        self.sns = SNSlogger(self.sns_topic, self.sns_subject, profile="tr-fr-sandbox")
+        session = boto3.session.Session(profile_name="tr-fr-sandbox",region_name="us-east-1")
+        self.cfn_client = session.client('cloudformation')
+        self.detections = self.check_drift()
+        self.sns.log.info("Detections: {}".format(self.detections))
+        self.wait_for_detection()
+        self.report()
+
+
+    def _get_stacks(self):
+        '''
+        Retreives all stacks in region
+        '''
+        resp = self._cfn_call('list_stacks',{'StackStatusFilter':ACTIVE_STACK_STATUS})
+        stacks = resp['StackSummaries']
+        while stacks == None or 'NextToken' in resp.keys():
+            resp = self._cfn_call('list_stacks',{'NextToken':resp['NextToken']})
+            stacks.append(resp['StackSummaries'])
+        return stacks
+
+
+    def check_drift(self,last_check_threshold=60):
+        '''
+        Checks every stack for drift
+        '''
+        detections = []
+        for stack in self._get_stacks():
+            resp = None
+            check_threshold = datetime.now(timezone.utc) - timedelta(seconds=last_check_threshold)
+            print('Stack: {}'.format(stack))
+            if stack.get('DriftInformation', {'StackDriftStatus':'NOT_CHECKED'})['StackDriftStatus'] == 'NOT_CHECKED':
+                resp = self._detect(stack['StackName'])
+            else:
+                if stack['DriftInformation']['LastCheckTimestamp'] < check_threshold:
+                    resp = self._detect(stack['StackName'])
+            if resp:
+                detections.append(resp)
+        return detections
+
+
+    def _detect(self,stack_name):
+        '''
+        Private method for making the detect request with exception handling
+        '''
+
+        try:
+            resp = self._cfn_call('detect_stack_drift',{'StackName':stack_name})
+            return resp['StackDriftDetectionId']
+        except ClientError as e:
+            if 'Drift detection is already in progress for stack' in e.response['Error']['Message']:
+                self.log.critical(e.response['Error']['Message'])
+            return None
+
+    def wait_for_detection(self,backoff=3,max_tries=3):
+        for detection_id in self.detections:
+            try_count = 0
+            detection_status = 'DETECTION_IN_PROGRESS'
+            while detection_status == 'DETECTION_IN_PROGRESS' and try_count <= max_tries:
+                print('Checking {}'.format(detection_id))
+                resp = self._cfn_call('describe_stack_drift_detection_status',{'StackDriftDetectionId':detection_id})
+                detection_status = resp['DetectionStatus']
+                if detection_status == 'DETECTION_IN_PROGRESS':
+                    try_count += 1
+                    sleep(backoff * try_count)
+            # Detections fail if a resource is not supported but we want to know if there's a failure other than that.
+            if detection_status == 'DETECTION_FAILED' and 'Failed to detect drift on resource' not in resp['DetectionStatusReason']:
+                self.sns.log.critical('Detection Failed, StackId: {}, Reason: {}'.format(resp['StackId'],resp['DetectionStatusReason']))
+                self.failed_stack_ids.append(resp['StackId'])
+
+    def report(self):
+        # report_obj = {}
+        for stack in self._get_stacks():
+            if stack['StackId'] not in self.failed_stack_ids:
+                print('Stack result: {}'.format(stack))
+                log_line = 'StackName: {}, DriftStatus: {}, LastCheckTimestamp: {}'.format(
+                    stack['StackName'],
+                    stack['DriftInformation']['StackDriftStatus'],
+                    stack['DriftInformation']['LastCheckTimestamp']
+                )
+                if stack['DriftInformation']['StackDriftStatus'] == 'DRIFTED':
+                    self.sns.log.critical(log_line)
+                else:
+                    self.sns.log.info(log_line)
+
+            #report_obj[stack['StackName']] = {
+            #    'DriftStatus': stack['DriftInformation']['StackDriftStatus'],
+            #    'LastCheckTimestamp': stack['DriftInformation']['LastCheckTimestamp'].isoformat(timespec='minutes')
+            #}
+
+    @retry(ClientError)
+    def _cfn_call(self,method,parameters={}):
+        '''
+        Put a retry decorator on any cfn method to avoid rate limit
+        '''
+        return getattr(self.cfn_client,method)(**parameters)
+
+def drift_region(profile,region):
+    dd = DriftDetector(profile,region)
+    return True
+
+def lambda_handler(event,context):
+    if isinstance(context,test_context):
+        profile = context.profile
+        regions = REGIONS if context.region == 'all' else [context.region]
+    else:
+        profile = None
+        regions = os.environ.get('REGIONS',REGIONS)
+    with Pool(len(regions)) as p:
+        p.starmap(drift_region,zip(repeat(profile),regions))
+
+
+class test_context(dict):
+    '''This is a text context object used when running function locally'''
+    def __init__(self,profile,region=None):
+        self.profile = profile
+        self.region = region
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Lambda Function that detects CloudFormation drift.')
+    parser.add_argument("-r","--region", help="Region in which to run. Default: all (will run for all regions)", default='all')
+    parser.add_argument("-p","--profile", help="Profile name to use when connecting to aws.", default=None)
+
+    args = parser.parse_args()
+
+    event = {}
+    context = test_context(args.profile,args.region)
+
+    lambda_handler(event,context)
---
+++ stack-drift-notifier/lambda/drift_detection/sns_logger.py
@@ -0,0 +1,45 @@
+import boto3
+import logging
+import re
+
+class SNSLogHandler(logging.Handler):
+    def __init__(self, topic, subject, profile="tr-fr-sandbox"):
+        logging.Handler.__init__(self)
+        region = re.search('arn:\w+:\w+:(.+?):\d+:.+$', topic).group(1)
+        self.session = boto3.session.Session(profile_name="tr-fr-sandbox",region_name="us-east-1")
+        self.sns_client = self.session.client('sns')
+        self.topic = topic
+        self.subject = subject
+
+    def emit(self, record):
+        self.sns_client.publish(TopicArn=self.topic, Message=record.message, Subject=self.subject)
+
+class SNSlogger(object):
+    def __init__(self, sns_topic_id, sns_topic_subject, profile="tr-fr-sandbox"):
+        self.sns_topic = sns_topic_id
+        self.sns_subject = sns_topic_subject
+        self.profile = profile
+        self._init_logging()
+
+    def _init_logging(self):
+        self.log = logging.getLogger('SNS_Logger')
+
+        # Should set the level on the logger itself to DEBUG
+        # and let the handlers below do the filtering
+        self.log.setLevel(logging.DEBUG)
+        # Setting console output to DEBUG for easier debugging
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        # THIS NEEDS TO BE CHANGED.
+        hdlr = logging.FileHandler('/tmp/sns.log')
+        hdlr.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        self.log.addHandler(ch)
+        self.log.addHandler(hdlr)
+        sns = SNSLogHandler(self.sns_topic, self.sns_subject, self.profile)
+
+        # We only want critical messages bothering us via AWS SNS
+        sns.setLevel(logging.CRITICAL)
+        sns.setFormatter(formatter)
+        self.log.addHandler(sns)
